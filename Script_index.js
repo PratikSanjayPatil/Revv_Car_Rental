d9f48c42ab9021ea7a0f6f4791ab8adefc6d7297
PratikSanjayPatil <pratikspatil3040@gmail.com>
2023-11-09 23:50:12 +0530

    Improve Code

--- Script_index.js
+++ Script_index.js
@@ -1,13 +1,15 @@
-console.log("index");
+
 let menubtn = document.getElementById("menu_btn")
 menubtn.addEventListener("click",()=>{
     opnsdmenu(true);
 })
-let menuclosebtn = document.getElementById("menu_close_btn")
-menuclosebtn.addEventListener("click",()=>{
-    opnsdmenu(false);
-})
+
 const opnsdmenu = (flag)=>{
     let sidemenu = document.getElementById("side_menu")
     sidemenu.className = flag ? "active" : ""
 }
+
+let menuclosebtn = document.getElementById("menu_close_btn")
+menuclosebtn.addEventListener("click",()=>{
+    opnsdmenu(false);
+})
